6028b7dba962c2ea444345e064f95accc8588a28
Mateus Wesley <mateuswesley314@gmail.com>
2022-12-19 18:03:19 -0300

    Create ex050.rb

---
+++ ex050.rb
@@ -0,0 +1,28 @@
+class MyCar
+
+  def initialize(year, color, model)
+    @year = year
+    @color = color
+    @model = model
+    @velocity = 0
+    @on = false
+  end
+
+  def change_speed(delta_v)
+    @velocity += delta_v
+  end
+
+  def brake(delta_v)
+    @velocity -= delta_v
+  end
+
+  def velocidade_atual
+    puts "Velocidade atual: #{@velocity} km/h"
+  end
+
+  def shut_off_on
+    @on = !@on
+    @velocity = 0
+  end
+
+end
