c49429fa95f1b08f35c76eec1255fd2ad07d84c7
Mateus Wesley <mateuswesley314@gmail.com>
2023-01-02 18:22:50 -0300

    Update event_manager.rb
    formating number
    getting hour with hightest frequency
    getting day with hightest frequency

--- serialization/event_manager/lib/event_manager.rb
+++ serialization/event_manager/lib/event_manager.rb
@@ -23,7 +23,7 @@ contents_array.each_with_index do |line, i|
 require 'csv'
 require 'erb'
 require 'google/apis/civicinfo_v2'
-
+require 'time'
 
 template = ERB.new File.read('form_letter.erb')
 
@@ -51,9 +51,9 @@ end
 def clean_phone(phone)
   phone = phone.to_s.scan(/\d+/).join
   if phone.length == 11 && phone.to_s[0] == '1'
-    phone[1..]
+    "(#{phone[1..3]}) #{phone[4..6]}-#{phone[7..]}"
   elsif phone.length == 10
-    phone
+    "(#{phone[0..2]}) #{phone[3..5]}-#{phone[6..]}"
   else
     'The number is invalid'
   end
@@ -70,12 +70,39 @@ def save_thank_you_letter(id, form_letter)
   end
 end
 
-# dados do participantes do evento em CSV
-contents = CSV.open(
-  'event_attendees.csv',
-  headers: true,
-  header_converters: :symbol
-)
+def get_csv_data(arquive, headers, header_converters)
+  CSV.open(
+    arquive,
+    headers: headers,
+    header_converters: header_converters
+  )
+end
+
+def create_time_to_object(time_string, time_format)
+  Time.strptime(time_string, time_format)
+end
+
+# create a hash with the values of an array and
+# their frequence
+def counting_values(values_array)
+  values_hash = Hash.new
+  values_array.uniq.each do |value|
+    values_hash[value] = values_array.count(value)
+  end
+  values_hash
+end
+
+# get the key with the hights value
+def highest_value_hash(hash)
+  hash.key(hash.values.max)
+end
+
+# getting csv content with the data from registred people
+contents = get_csv_data('event_attendees.csv', true, :symbol)
+
+# self explanatory
+hour_array = []
+day_array = []
 
 contents.each do |row|
   # for each person get the data
@@ -83,12 +110,29 @@ contents.each do |row|
   name = row[:first_name]
   zip = clean_zip(row[:zipcode])
   phone = clean_phone(row[:homephone])
-  puts "#{id} #{name} #{zip} #{phone}"
+  date = create_time_to_object(row[:regdate], '%m/%d/%Y %k:%M')
+  hour_array << date.hour
+  day_array << date.strftime('%A')
+
+  puts "#{id} #{name} #{zip} #{phone} #{hour_array[-1]} #{day_array[-1]}"
+
   # use the data from csv to search for person in API
   legislators = legislators_by_zipcode(zip)
 
   form_letter = template.result(binding)
-  puts form_letter
-  save_thank_you_letter(id, form_letter)
 
+  # puts form_letter
+  save_thank_you_letter(id, form_letter)
 end
+
+# Array with the hour of the day in the 24hrs format as keys and the frequencie as values
+hour_freq = counting_values(hour_array)
+day_freq = counting_values(day_array)
+
+# getting best hour for ad publicity
+best_hour = highest_value_hash hour_freq
+puts "The time with the highest number of records is between #{best_hour}:00 and #{best_hour}:59"
+
+# getting day with more registers
+best_day = highest_value_hash day_freq
+puts "The day with the highest number of records is #{best_day}"
