22d9a570e9ec6164407c2922b702abcb8a55ce79
Mateus Wesley <mateuswesley314@gmail.com>
2023-01-12 18:19:01 -0300

    implementando um metodo each
    Implementando um metodo each com os conceitos de blocos e yield

---
+++ ex058.rb
@@ -0,0 +1,28 @@
+## Implementando um metodo each usando o conceito de block e yield
+
+
+# Criando uma classe que representa uma estrutura do tipo array
+class Sarray
+  attr_accessor :values
+
+  # recebe os valores quebra eles e transforma numa variavel de instancia com
+  # esses valores
+  def initialize(*values)
+    @values = values
+  end
+
+  # para cada valor de @values
+  # para o metodo e executa o bloco dado como argumento do metodo each
+  def each
+    i = 0
+    for i in 0...@values.length
+      yield(@values[i])
+    end
+  end
+
+
+end
+
+array = Sarray.new(1, 2, 3, 10, 11, 54).values
+array.each {|value| puts value*2}
+
